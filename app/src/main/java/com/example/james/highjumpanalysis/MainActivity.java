e5d802eb94350d1ad9edf18e9c936655577a8ce0
Elson9 <jelson392@gmail.com>
2018-11-19 07:17:45 -0500

    Added video capture functionality.

--- app/src/main/java/com/example/james/highjumpanalysis/MainActivity.java
+++ app/src/main/java/com/example/james/highjumpanalysis/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.james.highjumpanalysis;
 
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    static final int REQUEST_VIDEO_CAPTURE = 1;
+
+    // Method to capture video
+    public void dispatchTakeVideoIntent(View view) {
+        Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        if (takeVideoIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
+        }
     }
 }
